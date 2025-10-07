e7d31cce03263f3bf755e24a25e7e21a45bd35f6
yurisantanaa <yuri.santana@discente.ufg.br>
2025-10-07 11:43:21 -0300

    .

--- prova1/teste.c
+++ prova1/teste.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
-extern remove_negativo(int *arr, size_t size);
+extern int remove_negativo(int *arr, size_t size);
 
 int main(){
   int a[] = {1,2,3,4,5,-6,-7,8,0};
